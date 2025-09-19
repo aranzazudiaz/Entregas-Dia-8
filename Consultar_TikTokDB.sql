50ebdcdf21063afedc52bdfd69f1c2c3ab9edf81
aranzazudiaz <arantza.diazmedina@gmail.com>
2025-09-19 13:59:08 +0200

    Add files via upload

---
+++ Consultar_TikTokDB.sql
@@ -0,0 +1,25 @@
+-- Consultar datos
+
+-- Query 1
+SELECT * FROM usuarios;
+
+-- Query 2 
+SELECT * FROM videos;
+
+-- Query 3
+SELECT * FROM comentarios;
+
+-- Query 4
+SELECT * FROM likes;
+
+-- Query 5
+SELECT * FROM seguidores;
+
+-- Query 6
+SELECT pais FROM usuarios;
+
+-- Query 7
+SELECT titulo FROM videos;
+
+-- Query 8
+SELECT texto FROM comentarios;
